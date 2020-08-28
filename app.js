029cad4948baaa23ebfe9f40b497aae36a017c80
Luis Altuve <luis2ra@gmail.com>
2020-08-28 15:50:53 -0400

    Práctica de video 01 de curso de Node.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const frutas = ["manzana", "pera", "durazno"]
+
+frutas.forEach((fruta) => {
+    console.log(fruta);
+    //console.count(fruta);
+});
+
+
+frutas.forEach((fruta) => {
+    //console.log(fruta);
+    console.count(fruta);
+});
