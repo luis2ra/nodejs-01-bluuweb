76fa911141d17ab8f2f433b6647b091adb431e3a
Luis Altuve <luis2ra@gmail.com>
2020-08-29 11:48:02 -0400

    Video-03 de Node.js: package.json y NPM

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const { fruit, money } = require("./frutas");
 
+const cowsay = require('cowsay');
+
 fruit.forEach((fruitsss) => {
     //console.log(fruta);
     console.count(fruitsss);
 });
 
 console.log(money);
+
+console.log(cowsay.say({
+	text : "I'm a moodule",
+	e : "oO",
+	T : "U "
+}));
+
